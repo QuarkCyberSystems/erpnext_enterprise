2lambda123/frappe-erpnext

feat(analytics): comply erpnext with utm methodology

erpnext/accounts/doctype/pricing_rule/test_pricing_rule.py
@@ -1300,7 +1300,7 @@ class TestPricingRule(unittest.TestCase):
 			item_code_rule.delete()
 
 
-test_dependencies = ["Campaign"]
+test_dependencies = ["Marketing Campaign"]
 
 
 def make_pricing_rule(**args):
@@ -1360,9 +1360,9 @@ def make_pricing_rule(**args):
 
 
 def setup_pricing_rule_data():
-	if not frappe.db.exists("Campaign", "_Test Campaign"):
+	if not frappe.db.exists("Marketing Campaign", "_Test Campaign"):
 		frappe.get_doc(
-			{"doctype": "Campaign", "campaign_name": "_Test Campaign", "name": "_Test Campaign"}
+			{"doctype": "Marketing Campaign", "description": "_Test Campaign", "name": "_Test Campaign"}
 		).insert()
 
 

erpnext/crm/doctype/campaign/campaign.py
@@ -25,6 +25,24 @@ class Campaign(Document):
 		naming_series: DF.Literal["SAL-CAM-.YYYY.-"]
 	# end: auto-generated types
 
+	def after_insert(self):
+		try:
+			mc = frappe.get_doc("Marketing Campaign", self.campaign_name)
+		except frappe.DoesNotExistError:
+			mc = frappe.new_doc("Marketing Campaign")
+			mc.name = self.campaign_name
+		mc.crm_campaign = self.campaign_name
+		mc.save(ignore_permissions=True)
+
+	def on_change(self):
+		try:
+			mc = frappe.get_doc("Marketing Campaign", self.campaign_name)
+		except frappe.DoesNotExistError:
+			mc = frappe.new_doc("Marketing Campaign")
+			mc.name = self.campaign_name
+		mc.crm_campaign = self.campaign_name
+		mc.save(ignore_permissions=True)
+
 	def autoname(self):
 		if frappe.defaults.get_global_default("campaign_naming_by") != "Naming Series":
 			self.name = self.campaign_name

erpnext/crm/report/campaign_efficiency/campaign_efficiency.py
@@ -9,8 +9,8 @@ from frappe.utils import flt
 
 def execute(filters=None):
 	columns, data = [], []
-	columns = get_columns("Campaign Name")
-	data = get_lead_data(filters or {}, "Campaign Name")
+	columns = get_columns("Campaign")
+	data = get_lead_data(filters or {}, "Campaign")
 	return columns, data
 
 

QuarkCyberSystems/erpnext_enterprise

erpnext/accounts/doctype/pricing_rule/test_pricing_rule.py
@@ -1377,7 +1377,7 @@ class TestPricingRule(unittest.TestCase):
 		pi.cancel()
 
 
-test_dependencies = ["Campaign"]
+test_dependencies = ["UTM Campaign"]
 
 
 def make_pricing_rule(**args):
@@ -1437,9 +1437,9 @@ def make_pricing_rule(**args):
 
 
 def setup_pricing_rule_data():
-	if not frappe.db.exists("Campaign", "_Test Campaign"):
+	if not frappe.db.exists("UTM Campaign", "_Test Campaign"):
 		frappe.get_doc(
-			{"doctype": "Campaign", "campaign_name": "_Test Campaign", "name": "_Test Campaign"}
+			{"doctype": "UTM Campaign", "description": "_Test Campaign", "name": "_Test Campaign"}
 		).insert()
 
 

erpnext/crm/doctype/campaign/campaign.py
@@ -25,6 +25,26 @@ class Campaign(Document):
 		naming_series: DF.Literal["SAL-CAM-.YYYY.-"]
 	# end: auto-generated types
 
+	def after_insert(self):
+		try:
+			mc = frappe.get_doc("UTM Campaign", self.campaign_name)
+		except frappe.DoesNotExistError:
+			mc = frappe.new_doc("UTM Campaign")
+			mc.name = self.campaign_name
+		mc.campaign_description = self.description
+		mc.crm_campaign = self.campaign_name
+		mc.save(ignore_permissions=True)
+
+	def on_change(self):
+		try:
+			mc = frappe.get_doc("UTM Campaign", self.campaign_name)
+		except frappe.DoesNotExistError:
+			mc = frappe.new_doc("UTM Campaign")
+			mc.name = self.campaign_name
+		mc.campaign_description = self.description
+		mc.crm_campaign = self.campaign_name
+		mc.save(ignore_permissions=True)
+
 	def autoname(self):
 		if frappe.defaults.get_global_default("campaign_naming_by") != "Naming Series":
 			self.name = self.campaign_name

erpnext/crm/report/campaign_efficiency/campaign_efficiency.py
@@ -9,8 +9,8 @@ from frappe.utils import flt
 
 def execute(filters=None):
 	columns, data = [], []
-	columns = get_columns("Campaign Name")
-	data = get_lead_data(filters or {}, "Campaign Name")
+	columns = get_columns("utm_campaign")
+	data = get_lead_data(filters or {}, "utm_campaign")
 	return columns, data
 
 